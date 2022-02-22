d59ff67be6f53b4c90674839b22e96ce63a7fd14
iwezfuf <80110958+iwezfuf@users.noreply.github.com>
2022-02-22 21:58:57 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import math
+import time
 import pygame
 import pygame.freetype
 from pygame.locals import (
@@ -65,8 +66,13 @@ class Player(pygame.sprite.Sprite):
         self.score = 0 
         players.add(self)
 
-    def update(self):
-        if self == player1:
+    def update(self, type):
+        if type == 1 and self == player2:
+            if ball.rect.center[1] > self.rect.center[1]:
+                self.rect.move_ip(0, self.speed)
+            elif ball.rect.center[1] < self.rect.center[1]:
+                self.rect.move_ip(0, -self.speed)
+        else: 
             pressed_keys = pygame.key.get_pressed()
             if pressed_keys[self.keys[0]]:
                 self.rect.move_ip(0, -self.speed)
@@ -77,12 +83,6 @@ class Player(pygame.sprite.Sprite):
                 self.rect.top = 0
             if self.rect.bottom > SCREEN_HEIGHT:
                 self.rect.bottom = SCREEN_HEIGHT
-                
-        elif bp == 1 and self == player2:
-            if ball.rect.center[1] > self.rect.center[1]:
-                self.rect.move_ip(0, self.speed)
-            elif ball.rect.center[1] < self.rect.center[1]:
-                self.rect.move_ip(0, -self.speed)
         
 
 balls = pygame.sprite.Group()
@@ -92,51 +92,115 @@ player1 = Player([SCREEN_WIDTH-10, SCREEN_HEIGHT//2-25], [K_UP, K_DOWN])
 player2 = Player([10,SCREEN_HEIGHT//2-25], [K_w, K_s])
 clock = pygame.time.Clock()
 
-running = True
-bp = False
-myfont = pygame.font.SysFont('Arial', 30)
-textsurface = myfont.render("2 Players", False, [255,255,255])
-textsurface2 = myfont.render("1 Player", False, [255,255,255])
-screen.fill((0,0,0))
-pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2-40,150,80],2)
-pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2+50,SCREEN_HEIGHT//2-40,150,80],2)
-screen.blit(textsurface, [SCREEN_WIDTH//2-180,SCREEN_HEIGHT//2-20])
-screen.blit(textsurface2, [SCREEN_WIDTH//2+70,SCREEN_HEIGHT//2-20])
-        
-while running:
-    if not bp:
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if pos[0] in range(SCREEN_WIDTH//2-180, SCREEN_WIDTH//2-30) and pos[1] in range(SCREEN_HEIGHT//2-40, SCREEN_HEIGHT//2+40):
-                    bp = 2
-                elif pos[0] in range(SCREEN_WIDTH//2+70, SCREEN_WIDTH//2+220)  and pos[1] in range(SCREEN_HEIGHT//2-40, SCREEN_HEIGHT//2+40):
-                    bp = 1
-            if event.type == QUIT:
-                running = False
-                pygame.display.quit()
-    else:    
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-                pygame.display.quit()
-                
-        screen.fill((0,0,0))
-        for ball in balls:
-            ball.update()
 
-        for player in players:
-            if player == player1:
-                pos = [100,100]
-            else: pos = [700,100]
-            myfont = pygame.font.SysFont('Arial', 100)
-            textsurface = myfont.render(str(player.score), False, [255,255,255])
-            screen.blit(textsurface, pos)
-            player.update()
-            screen.blit(player.surf, player.rect)
-            if player.score == 10:
-                running = False
-                
-    pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-1,0,2,SCREEN_HEIGHT])
-    pygame.display.flip()
-    clock.tick(60)
+class Game():
+    def __init__(self):
+        self.running = True
+        self.game(self.introScreen())
+
+    
+    def introScreen(self):
+        myfont = pygame.font.SysFont('Arial', 30)
+        textsurface = myfont.render("2 Players", False, [255,255,255])
+        textsurface2 = myfont.render("1 Player", False, [255,255,255])
+        screen.fill((0,0,0))
+        pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-200,SCREEN_HEIGHT//2-40,150,80],2)
+        pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2+50,SCREEN_HEIGHT//2-40,150,80],2)
+        screen.blit(textsurface, [SCREEN_WIDTH//2-180,SCREEN_HEIGHT//2-20])
+        screen.blit(textsurface2, [SCREEN_WIDTH//2+70,SCREEN_HEIGHT//2-20])
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    if pos[0] in range(SCREEN_WIDTH//2-180, SCREEN_WIDTH//2-30) and pos[1] in range(SCREEN_HEIGHT//2-40, SCREEN_HEIGHT//2+40):
+                        self.type = 2
+                        self.cooldownScreen(5000)
+                    elif pos[0] in range(SCREEN_WIDTH//2+70, SCREEN_WIDTH//2+220)  and pos[1] in range(SCREEN_HEIGHT//2-40, SCREEN_HEIGHT//2+40):
+                        self.type = 1
+                        self.cooldownScreen(1)
+                if event.type == QUIT:
+                    self.running = False
+                    pygame.display.quit()
+            
+            pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-1,0,2,SCREEN_HEIGHT])
+            pygame.display.flip()
+            clock.tick(60)
+    
+
+    def cooldownScreen(self, cooldown):
+        cooldownEvent = pygame.USEREVENT + 1
+        pygame.time.set_timer(cooldownEvent, cooldown)
+        start_ticks = pygame.time.get_ticks()
+        myfont = pygame.font.SysFont('Arial', 70)
+        gamestartsin = myfont.render(("Game starts in:"), False, [255,255,255])
+        while True:
+            screen.fill((0,0,0))
+            text = (cooldown - (pygame.time.get_ticks()-start_ticks))/1000
+            textsurface = myfont.render((str(text)), False, [255,255,255])
+            screen.blit(textsurface, [315,320])
+            screen.blit(gamestartsin, [200, 250])
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.display.quit()
+                if event.type == cooldownEvent:
+                    self.game()
+
+            pygame.display.flip()
+            clock.tick(60)
+
+    def game(self):
+        pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-1,0,2,SCREEN_HEIGHT])
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.display.quit()
+   
+            screen.fill((0,0,0))
+            for ball in balls:
+                ball.update()
+
+            for player in players:
+                if player == player1:
+                    pos = [100,100]
+                else: pos = [700,100]
+                myfont = pygame.font.SysFont('Arial', 100)
+                textsurface = myfont.render(str(player.score), False, [255,255,255])
+                screen.blit(textsurface, pos)
+                player.update(self.type)
+                screen.blit(player.surf, player.rect)
+                if player.score == 3:
+                    if player == player1:
+                        self.endScreen(1)
+                    else: self.endScreen(2)
+            
+            pygame.draw.rect(screen, [255,255,255], [SCREEN_WIDTH//2-1,0,2,SCREEN_HEIGHT])
+            pygame.display.flip()
+            clock.tick(60)
+                    
+    
+    def endScreen(self, winner):
+        myfont = pygame.font.SysFont('Arial', 70)
+        winnertext = myfont.render(("Player " + str(winner) + " won!"), False, [255,255,255])
+        anothergame = myfont.render(("New game"), False, [255,255,255])
+        while True:
+            screen.fill((0,0,0))
+            screen.blit(winnertext, [220, 240])
+            screen.blit(anothergame, [260, 360])
+            pygame.draw.rect(screen, [255,255,255], [240,360,318,90],2)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.display.quit()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    if pos[0] in range(240, 240+318) and pos[1] in range(360, 360+90):
+                        player1.score = 0
+                        player2.score = 0
+                        self.introScreen()
+
+            pygame.display.flip()
+            clock.tick(60)
+
+
+
+game = Game()
